ed52d7b9570dafef220b112b20bef32d4f3bbae0
andreaolmos90 <andreaolmos90@gmail.com>
2021-11-13 19:50:03 -0300

    creo clase01

--- src/bftf/951_dataset_epic_modificado_para_dataset_pruebas.r
+++ src/bftf/951_dataset_epic_modificado_para_dataset_pruebas.r
@@ -654,6 +654,7 @@ correr_todo  <- function( palancas )
   #genero dataset para pruebas locales
   ktrain_subsampling = 0.02 ## tamaño de la muestra
   vector_azar  <- runif( nrow(dataset) ) ## defino un vector de probas con todo el dataset
+  dataset[ , clase01:= ifelse( clase_ternaria=="CONTINUA", 0, 1 ) ] #creo la clase01
   dataset[ ( clase01==1 | vector_azar < ktrain_subsampling ), ] ## recorto el dataset (solo clase continua)
   
   #Grabo el dataset recortado
