12de8030147e00821153eb1d1d8838d2f9a6e29b
andreaolmos90 <andreaolmos90@gmail.com>
2021-11-22 14:46:21 -0300

    oris and rankings con palanca de canaritos corregida y archivo de importancias con nombre de palancas

--- src/bftf/951_dataset_epic_02_ori_and_ranking.r
+++ src/bftf/951_dataset_epic_02_ori_and_ranking.r
@@ -593,7 +593,7 @@ CanaritosImportancia  <- function( dataset )
   tb_importancia  <- lgb.importance( model= modelo )
   tb_importancia[  , pos := .I ]
   
-  fwrite( tb_importancia, file="./work/impo.txt", sep="\t" )
+  fwrite( tb_importancia, file=paste0("./work/impo_", palancas$version ,".txt"), sep="\t" )
   
   umbral  <- tb_importancia[ Feature %like% "canarito", median(pos) - sd(pos) ]
   col_inutiles  <- tb_importancia[ pos >= umbral | Feature %like% "canarito",  Feature ]
@@ -724,7 +724,7 @@ correr_todo  <- function( palancas )
     }
 
 
-  #dejo la clase como ultimo campo
+  #dejo la clase como ultimo campo´
   nuevo_orden  <- c( setdiff( colnames( dataset ) , "clase_ternaria" ) , "clase_ternaria" )
   setcolorder( dataset, nuevo_orden )
 
